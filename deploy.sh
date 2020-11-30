a7bc36494f7b96c6560acb8f9733e48b7a32ed1d
James Lucas <73145226+jlucasdba@users.noreply.github.com>
2020-11-29 21:30:37 -0600

    Add theme option to deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+#hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t purehugo
 
 # Go To Public folder
 cd public
